de3725a1458ea24d8bd0378320bb54143a3c2d2a
tacherasasi <tacherasasi@gmail.com>
2025-06-19 10:45:35 +0300

    Processing users guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number");
+
+    println!("Please enter you guess");
+
+    let mut guess = String::new();
+    let mut output_msg = "You have guessed {}";
+
+    let err_msg = "Something went wrong. {}";
+    io::stdin().read_line(&mut guess).expect(err_msg);
+
+    println!(output_msg, guess)
 }
