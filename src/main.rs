090f02f1ff079a750252d6246e1df42fa7e740ea
tacherasasi <tacherasasi@gmail.com>
2025-06-20 17:48:43 +0300

    Ellie: added a while loop and is_match bool

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,24 @@ fn main() {
     let mut guess = String::new();
     let rand_number = rand::rng().random_range(1..=100);
     let mut _output_msg = "You have guessed {}";
+    let mut is_match: bool = false;
 
     let err_msg = "Something went wrong. {}";
-    io::stdin().read_line(&mut guess).expect(err_msg);
-
-    println!("You have guessed {}", guess);
-
-    match guess.cmp(&rand_number.to_string()){
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!")
+    while(!is_match){
+        io::stdin().read_line(&mut guess).expect(err_msg);
+        
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    
+        println!("You have guessed {}", guess);
+        if(guess == rand_number){
+            println!("You have guessed right, You win!");
+            is_match = true;
+        }
+    
+        match guess.cmp(&rand_number){
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => println!("You win!")
+        }
     }
 }
