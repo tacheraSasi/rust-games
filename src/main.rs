c1ecdece7e8f3e65144a94d578ae23f7379d2ea0
tacherasasi <tacherasasi@gmail.com>
2025-06-20 11:40:45 +0300

    Ellie: added the rand_number variable

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use std::io;
 fn main() {
     println!("Guess a number");
 
-    print!("Please enter you guess ");
+    println!("Please enter you guess");
 
     let mut guess = String::new();
-    let mut output_msg = "You have guessed {}";
+    let mut _output_msg = "You have guessed {}";
 
     let err_msg = "Something went wrong. {}";
     io::stdin().read_line(&mut guess).expect(err_msg);
