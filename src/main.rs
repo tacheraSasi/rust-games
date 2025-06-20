db99074363bb43b3cca70310227c45432d81a658
tacherasasi <tacherasasi@gmail.com>
2025-06-20 17:32:42 +0300

    Ellie: convert the rand number to string so that i can compare it to the guess input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 use std::cmp::Ordering;
-use rand::rng;
+use rand::Rng;
 
 fn main() {
     println!("Guess a number");
@@ -8,15 +8,15 @@ fn main() {
     println!("Please enter you guess");
 
     let mut guess = String::new();
-    let rand_number = rand::thread_rng().gen_range(1..=100)
+    let rand_number = rand::thread_rng().gen_range(1..=100);
     let mut _output_msg = "You have guessed {}";
 
     let err_msg = "Something went wrong. {}";
     io::stdin().read_line(&mut guess).expect(err_msg);
 
-    println!("You have guessed {}", guess)
-    
-    match guess.cmp(&rand_number){
+    println!("You have guessed {}", guess);
+
+    match guess.cmp(&rand_number.to_string()){
         Ordering::Less => println!("Too small"),
         Ordering::Greater => println!("Too big"),
         Ordering::Equal => println!("You win!")
