b02429172f3c9a03897496d952f4bf2ec5cd8b32
tacherasasi <tacherasasi@gmail.com>
2025-06-20 17:59:18 +0300

    Ellie: used a loop{} instead of while{} and breaked out of the loop after a user has won

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,30 @@ fn main() {
 
     println!("Please enter you guess");
 
-    let mut guess = String::new();
     let rand_number = rand::rng().random_range(1..=100);
     let mut _output_msg = "You have guessed {}";
-    let mut is_match: bool = false;
 
     let err_msg = "Something went wrong. {}";
-    while(!is_match){
+    loop {
+         
+        let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect(err_msg);
         
         let guess: u32 = guess.trim().parse().expect("Please type a number!");
     
         println!("You have guessed {}", guess);
-        if(guess == rand_number){
-            println!("You have guessed right, You win!");
-            is_match = true;
-        }
+        // if guess == rand_number {
+        //     println!("You have guessed right, You win!");
+        //     break;
+        // }
     
         match guess.cmp(&rand_number){
             Ordering::Less => println!("Too small"),
             Ordering::Greater => println!("Too big"),
-            Ordering::Equal => println!("You win!")
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
