401aad6e1900aea2b819dec521eae5085f6658c5
tacherasasi <tacherasasi@gmail.com>
2025-06-20 15:14:10 +0300

    Ellie: more changes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::rng;
 
 fn main() {
     println!("Guess a number");
@@ -6,6 +7,7 @@ fn main() {
     println!("Please enter you guess");
 
     let mut guess = String::new();
+    let rand_number = rand::thread_rng().gen_range(1..=100)
     let mut _output_msg = "You have guessed {}";
 
     let err_msg = "Something went wrong. {}";
